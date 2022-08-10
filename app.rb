cfceea29992a6a609d1345b06dab640e43848d8f
EduardoBriceno89 <eduardbrice@outlook.com>
2022-08-10 18:01:18 -0500

    Add matrix class

---
+++ app.rb
@@ -0,0 +1,25 @@
+class Matrix
+    def initialize(row = 0, columns = 0)
+        @row = row
+        @columns = columns
+    end
+
+    def row
+        @row
+    end
+
+    def column
+        @column
+    end
+
+    
+
+end
+
+class iniciar_juego < Matrix
+    def show_matrix
+        # mostrar matriz
+
+    end
+    # todo el algoritmo
+end
